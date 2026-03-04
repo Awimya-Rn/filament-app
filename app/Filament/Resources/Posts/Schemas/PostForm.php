fe72c9e90fc0184de2ae036b14af4412a6ccff09
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-04 19:25:58 +0700

    jobsheet filamen5-Menggunakan Section

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -20,7 +20,10 @@ class PostForm
     {
         return $schema
             ->components([
-                Section::make('Post Details')->schema([
+                Section::make('Post Details')
+                ->Description('Fiil in the details of the post.')
+                ->icon('heroicon-o-document-text')
+                ->schema([
                     TextInput::make('title')->required()->minLength(5),
                     TextInput::make('slug')->required()->unique(ignoreRecord:true),
                     Select::make('category_id')
