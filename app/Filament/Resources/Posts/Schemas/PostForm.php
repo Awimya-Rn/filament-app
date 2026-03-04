1741a31a518b1a87d715cf8b7f1b6a38ab6b6403
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-04 20:08:01 +0700

    jobsheet filamen5-Mengatur Lebar Field Individual

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -25,13 +25,15 @@ class PostForm
                 ->Description('Fiil in the details of the post.')
                 ->icon('heroicon-o-document-text')
                 ->schema([
-                    TextInput::make('title')->required()->minLength(5),
-                    TextInput::make('slug')->required()->unique(ignoreRecord:true),
-                    Select::make('category_id')
-                        ->relationship('category','name')
-                        ->preload()
-                        ->searchable(),
-                    ColorPicker::make('color'),
+                    Group::make([
+                        TextInput::make('title')->required()->minLength(5),
+                        TextInput::make('slug')->required()->unique(ignoreRecord:true),
+                        Select::make('category_id')
+                            ->relationship('category','name')
+                            ->preload()
+                            ->searchable(),
+                        ColorPicker::make('color'),
+                    ])->columns(2),
                     MarkdownEditor::make('content'),
                 ])->columnSpan(2),
                 Group::make([
