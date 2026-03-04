85a3bb298e16332e97668754e25188cc05b9e4d0
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-04 20:25:52 +0700

    jobsheet filamen5-Tugas

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -22,34 +22,56 @@ class PostForm
         return $schema
             ->components([
                 Section::make('Post Details')
-                ->Description('Fiil in the details of the post.')
-                ->icon('heroicon-o-document-text')
-                ->schema([
-                    Group::make([
-                        TextInput::make('title')->required()->minLength(5),
-                        TextInput::make('slug')->required()->unique(ignoreRecord:true),
-                        Select::make('category_id')
-                            ->relationship('category','name')
-                            ->preload()
-                            ->searchable(),
-                        ColorPicker::make('color'),
-                    ])->columns(2),
-                    MarkdownEditor::make('content'),
-                ])->columnSpan(2),
+                    ->description('Fiil in the details of the post.')
+                    ->icon('heroicon-o-document-text')
+                    ->schema([
+                        Group::make([
+                            TextInput::make('title')
+                            ->required()
+                            ->minLength(5)
+                            ->label('Judul Post'),
+                            TextInput::make('slug')
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->label('Slug / URL'),
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->preload()
+                                ->searchable()
+                                ->label('Categori'),
+                            ColorPicker::make('color')
+                                ->label('Theme Color'),
+                        ])->columns(2),
+
+                        MarkdownEditor::make('content')
+                            ->label('Post Content')
+                            ->columnSpanFull(),
+                    ])->columnSpan(2),
+
                 Group::make([
-                    Section::make('Image Upload')->schema([
-                        FileUpload::make('image')
-                            ->disk('public')
-                            ->directory('posts'),
-                    ]),
-                    Section::make('Meta Information')->schema([
-                        // RichEditor::make('content'),
-                            TagsInput::make('tags'),
-                            Checkbox::make('published'),
-                            DateTimePicker::make('published_at'),
-                    ]),
+                    Section::make('Media')
+                        ->description('Upload featured image.')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('posts')
+                                ->image()
+                                ->label('Featured Image'),
+                        ]),
+
+                    Section::make('Meta & Status')
+                        ->description('Publication and label settings.')
+                        ->icon('heroicon-o-cog-6-tooth')
+                        ->schema([
+                            TagsInput::make('tags')
+                                ->label('Tags'),
+                            Checkbox::make('published')
+                                ->label('Publish Now'),
+                            DateTimePicker::make('published_at')
+                                ->label('Publication Time'),
+                        ]),
                 ])->columnSpan(1),
-            // ])->columns(3);
             ])->columns(3);
     }
 }
