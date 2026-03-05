fdabc5792a89ec115ed238ee0b353b5772b7bfc5
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-05 19:25:39 +0700

    jobsheet filamen6-Implementasi Wizard Form

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -2,6 +2,13 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Schema;
 
 class ProductForm
@@ -10,7 +17,38 @@ class ProductForm
     {
         return $schema
             ->components([
-                //
+                Wizard::make([
+                    Step::make('Product Info')
+                        ->description('Isi Informasi Produk')
+                        ->schema([
+                            Group::make([
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('sku')
+                                    ->required(),
+                            ])->columns(2),
+                            MarkdownEditor::make('description')
+                        ]),
+                    Step::make('Pricing & Stock')
+                        ->description('Isi harga dan jumlah stok')
+                        ->schema([
+                            TextInput::make('price')
+                                ->numeric()
+                                ->required(),
+                            TextInput::make('stock')
+                                ->numeric()
+                                ->required(),
+                        ]),
+                    Step::make('Media & Status')
+                        ->description('Upload gambar dan atur status')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('products'),
+                            Checkbox::make('is_active'),
+                            Checkbox::make('is_featured'),
+                        ]),
+                ])->columnSpanFull(),
             ]);
     }
 }
