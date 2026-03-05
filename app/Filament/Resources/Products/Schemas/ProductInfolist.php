a7fe78987fdc0848e3de0c80920a1d49c2eee6a1
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-05 20:31:50 +0700

    jobsheet filamen8-Membuat Section – Product Info

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -10,7 +12,22 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                //
+                Section::make('Product Info')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+                    ])
+                    ->columnSpanFull(),
             ]);
     }
 }
