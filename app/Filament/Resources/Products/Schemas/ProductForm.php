141d10f845cd81a56a2bef916c2541c83a64e041
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-05 20:26:51 +0700

    jobsheet filamen7-Menampilkan data pada table & tugas

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -21,41 +21,57 @@ class ProductForm
                 Wizard::make([
                     Step::make('Product Info')
                         ->description('Isi Informasi Produk')
+                        ->icon('heroicon-o-shopping-bag')
                         ->schema([
                             Group::make([
                                 TextInput::make('name')
                                     ->required(),
                                 TextInput::make('sku')
+                                    ->label('SKU (Stock Keeping Unit)')
                                     ->required(),
                             ])->columns(2),
                             MarkdownEditor::make('description')
                         ]),
+
                     Step::make('Pricing & Stock')
                         ->description('Isi harga dan jumlah stok')
+                        ->icon('heroicon-o-currency-dollar')
                         ->schema([
                             TextInput::make('price')
                                 ->numeric()
+                                ->prefix('Rp')
+                                ->minValue(1)
                                 ->required(),
                             TextInput::make('stock')
                                 ->numeric()
+                                ->minValue(0)
                                 ->required(),
-                        ]),
+                        ])->columns(2),
+
                     Step::make('Media & Status')
                         ->description('Upload gambar dan atur status')
+                        ->icon('heroicon-o-photo') 
                         ->schema([
                             FileUpload::make('image')
+                                ->image()
                                 ->disk('public')
-                                ->directory('products'),
-                            Checkbox::make('is_active'),
-                            Checkbox::make('is_featured'),
+                                ->directory('products')
+                                ->columnSpanFull(),
+                            Group::make([
+                                Checkbox::make('is_active')
+                                    ->label('Produk Aktif'),
+                                Checkbox::make('is_featured')
+                                    ->label('Produk Unggulan'),
+                            ])->columns(2),
                         ]),
-                ])->columnSpanFull()
-                ->submitAction(
-                    Action::make('save')
-                        ->label('Save Product')
-                        ->color('primary')
-                        ->submit('save')
-                ),
+                ])
+                    ->columnSpanFull()
+                    ->submitAction(
+                        Action::make('save')
+                            ->label('Save Product')
+                            ->color('primary')
+                            ->submit('save')
+                    ),
             ]);
     }
 }
--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -6,6 +6,8 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class ProductsTable
@@ -14,7 +16,19 @@ class ProductsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name'),
+                TextColumn::make('sku'),
+                TextColumn::make('price'),
+                TextColumn::make('stock'),
+                ImageColumn::make('image')
+                    ->disk('public'),
+                TextColumn::make('is_active')
+                    ->label('Status')
+                    ->badge()
+                    ->color(fn(bool $state): string => match ($state) {
+                        true => 'success',
+                        false => 'danger',  
+                    })->formatStateUsing(fn(bool $state): string => $state ? 'Active' : 'Inactive'),
             ])
             ->filters([
                 //
