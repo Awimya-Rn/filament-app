7b95f4e2ee373f1c886711500bc17a049337d8e2
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-05 21:22:00 +0700

    jobsheet filamen9-Implementasi Tabs

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -14,6 +16,62 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tabs')
+                ->tabs([
+                    Tab::make('Product Detail')
+                        ->icon('heroicon-o-academic-cap')
+                        ->schema([
+                            TextEntry::make('name')
+                                ->label('Product Name')
+                                ->weight('bold')
+                                ->color('primary'),
+                            TextEntry::make('sku')
+                                ->label('Product SKU')
+                                ->badge()
+                                ->color('success'),
+                            TextEntry::make('description')
+                                ->label('Product Description'),
+                            TextEntry::make('created_at')
+                                ->label('Product Creation Date')
+                                ->date('d M Y')
+                                ->color('info'),
+                        ]),
+                    Tab::make('Pricing & Stock')
+                        ->icon('heroicon-o-currency-dollar')
+                        ->badge('10')
+                        ->badgeColor('info')
+                        ->schema([
+                            TextEntry::make('price')
+                                ->label('Product Price')
+                                ->weight('bold')
+                                ->color('primary')
+                                ->icon('heroicon-o-currency-dollar'),
+                            TextEntry::make('stock')
+                                ->label('Product Stock'),
+                        ]),
+                    Tab::make('Media & Status')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            ImageEntry::make('image')
+                                ->label('Product Image')
+                                ->disk('public'),
+                            TextEntry::make('price')
+                                ->label('Product Price')
+                                ->weight('bold')
+                                ->color('primary')
+                                ->icon('heroicon-o-currency-dollar'),
+                            TextEntry::make('stock')
+                                ->label('Product Stock')
+                                ->weight('bold')
+                                ->color('primary'),
+                            IconEntry::make('is_active')
+                                ->label('Is Active?')
+                                ->boolean(),
+                            IconEntry::make('is_featured')
+                                ->label('Is Featured?')
+                                ->boolean(),
+                        ]),
+                ])->columnSpanFull(),
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
@@ -25,7 +83,7 @@ class ProductInfolist
                             ->label('Product ID'),
                         TextEntry::make('sku')
                             ->label('Product SKU')
-                            ->badge() 
+                            ->badge()
                             ->color('warning'),
                         TextEntry::make('description')
                             ->label('Product Description'),
