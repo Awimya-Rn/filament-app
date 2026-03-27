fceb2d10d403df6366680ae567dc0c16a59560f4
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-03-27 20:09:16 +0700

    jobsheet filamen11-Membuat Filter Berdasarkan Relasi (Kategori)

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -11,6 +11,7 @@ use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class PostsTable
@@ -43,6 +44,10 @@ class PostsTable
                                 fn ($query,$date)=>$query->whereDate('created_at', $date),
                             );
                         }),
+                SelectFilter::make('category_id')
+                    ->relationship('category','name')
+                    ->label('Category')
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
